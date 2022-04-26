887f548b7e1b45f217f287db79f0c025b18a8079
Kuliscene <adhvaith9@gmail.com>
2022-04-27 04:23:46 +0530

    landing page update with admin

--- src/Components/LandingPageOptions.js
+++ src/Components/LandingPageOptions.js
@@ -5,59 +5,62 @@ import { OptionCard } from "./OptionCard.js";
 import bookPoolPhoto from "../Assets/bookPool.jpg";
 
 const LandingPageOptionsStyled = styled.div`
-  display: flex;
-  flex-direction: column;
-  & .main {
-    display: flex;
-    flex-wrap: wrap;
-    gap: 2em;
-    margin-left: 7em;
-    margin-right: 7em;
-    justify-content: center;
-  }
-  & .link {
-    text-decoration: none;
-  }
+display: flex;
+	flex-direction: column;
+	& .main{
+		display:flex;
+		flex-wrap:wrap;
+		gap:2em;
+		justify-content:center;
+		align-items:center;
+		margin-left:7em;
+		margin-right:7em;
+	}
+	& .link{
+		text-decoration:none;
+	}
 `;
 
 const LandingPageOptions = ({ loggedin }) => {
-  return (
-    <LandingPageOptionsStyled>
-      {loggedin === "admin" ? (
-        <>
-          <p>USERS</p>
-          <p>BOOKS</p>
-          <p>TRANSACTIONS</p>
-          <p>REQUESTS</p>
-        </>
-      ) : (
-        <>
-          <Link className="link" to="/userslanding/bookpool">
-            <OptionCard name={"Book Pool"} />
-          </Link>
-          <Link className="link" to="/userslanding/profile">
-            <OptionCard name={"My Profile"} />
-          </Link>
-          <Link className="link" to="/userslanding/lendabook">
-            <OptionCard name={"Lend a Book"} />
-          </Link>
-          <Link className="link" to="/userslanding/requestabook">
-            <OptionCard name={"Request a Book"} />
-          </Link>
-          <p>APPLY FOR EXTENSION</p>
-          <Link className="link" to="/userslanding/requestsrecieved">
-            <OptionCard name={"Requests Recieved"} />
-          </Link>
-          <Link className="link" to="/userslanding/returnbook">
-            <OptionCard name={"Return a Book"} />
-          </Link>
-          <Link className="link" to="/userslanding/paydues">
-            <OptionCard name={"Pay Dues"} />
-          </Link>
-        </>
-      )}
-    </LandingPageOptionsStyled>
-  );
+	return (
+		<LandingPageOptionsStyled>
+			{loggedin === "admin" ? (
+				<>
+					<p>USERS</p>
+					<p>BOOKS</p>
+					<p>TRANSACTIONS</p>
+					<p>REQUESTS</p>
+				</>
+			) : (
+				<div className="main">
+					<Link className="link" to="/userslanding/bookpool">
+						<OptionCard name={"Book Pool"} photoURL={bookPoolPhoto} />
+					</Link>
+					<Link className="link" to="/userslanding/profile">
+						<OptionCard name={"My Profile"} />
+					</Link>
+					<Link className="link" to="/userslanding/lendabook">
+						<OptionCard name={"Lend a Book"} />
+					</Link>
+					<Link className="link" to="/userslanding/requestabook">
+						<OptionCard name={"Request a Book"} />
+					</Link>
+					<Link className="link" to="/userslanding">
+						<OptionCard name={"Apply for Extension"} />
+					</Link>
+					<Link className="link" to="/userslanding/requestsrecieved">
+						<OptionCard name={"Requests Recieved"} />
+					</Link>
+					<Link className="link" to="/userslanding/returnbook">
+						<OptionCard name={"Return a Book"} />
+					</Link>
+					<Link className="link" to="/userslanding/paydues">
+						<OptionCard name={"Pay Dues"} />
+					</Link>
+				</div>
+			)}
+		</LandingPageOptionsStyled>
+	);
 };
 
 export default LandingPageOptions;
